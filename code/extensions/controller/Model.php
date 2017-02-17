b74b4ea99def167386cff9cca0ce4dda3ceb95b9
Steve Wake <steve@moveforward.co.nz>
2017-02-18 10:37:11 +1300

    Migrating to SS 4

---
+++ code/extensions/controller/Model.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modular\Extensions;
+use SilverStripe\Core\Extension;
+
+/**
+ * Extension added to ModelController
+ *
+ * @package Modular\Extensions\Controller
+ */
+class ControllerExtension extends Extension {
+}
